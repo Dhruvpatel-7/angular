4f68d6580761fe560847e3bfd3fdece7016665a4
Dhruv Patel <alteemit2@gmail.com>
2024-10-04 12:20:32 +0530

    Title Of the section on the tutorial page change

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_di/di/useclass_forwardref.js
+++
@@ -1,5 +0,0 @@
-SomeProvider.ɵprov = /*@__PURE__*/ $r3$.ɵɵdefineInjectable({
-  token: SomeProvider,
-  factory: __ngFactoryType__ => SomeProviderImpl.ɵfac(__ngFactoryType__),
-  providedIn: 'root'
-});
--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_di/di/useclass_without_deps.js
+++
@@ -1,5 +0,0 @@
-MyService.ɵprov = /*@__PURE__*/ $r3$.ɵɵdefineInjectable({
-  token: MyService,
-  factory: __ngFactoryType__ => MyAlternateService.ɵfac(__ngFactoryType__),
-  providedIn: 'root'
-});
